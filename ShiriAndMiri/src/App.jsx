dfcd8bc1fb2b4bfc54f783950a3601500d836caf
shiri shahor <shirishahor@gmail.com>
2025-01-08 20:24:47 +0200

    LOGIN SIGNUP 1 2

--- ShiriAndMiri/src/App.jsx
+++ ShiriAndMiri/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-
+import Login from './Login'
+import SignUp from './SignUP'
 function App() {
   
 
   return (
     <>
-      <RentCar/>
-      <Color/>
+     {/*<Login/>*/}
+     <SignUp/>
     </>
   )
 }

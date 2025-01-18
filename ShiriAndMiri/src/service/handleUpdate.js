c9bf45a824a6d6dd5f5e6ba3c58daf6b28d4620d
shiri shahor <shirishahor@gmail.com>
2025-01-18 22:21:33 +0200

    handleUpdate

---
+++ ShiriAndMiri/src/service/handleUpdate.js
@@ -0,0 +1,21 @@
+
+import fetchData from "./FetchData.js";
+
+const handleUpdate=async (setData,newForm,path,field) =>{
+    try {
+        const updatedValue = { [field]: newForm[field] };
+        const updatedData = await fetchData(path, 'PATCH', updatedValue) || [];
+        console.log(updatedData[0]);
+
+        setData((prevData) =>
+            prevData.map((item) =>
+                item.id === id ?  updatedData  : item
+            )
+        );
+    } catch (e) {
+        console.error('Error fetching:', e);
+        alert('Error fetching data');
+    }
+    
+}
+export default handleUpdate;
